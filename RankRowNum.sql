cff9c0f7ec580d96563b86bae7ef1dacdf3f40c2
Kiran Kumar Madiraju <64897981+kkmadiraju@users.noreply.github.com>
2026-09-12 15:25:57 +0530

    Update RankRowNum.sql

--- RankRowNum.sql
+++ RankRowNum.sql
@@ -1,1 +1,6 @@
+--Use online SQL practise
+--https://www.programiz.com/sql/online-compiler
 
+select item,amount, rank() over (order  by amount asc) as rankamt from orders;
+select item,amount, dense_rank() over (order  by amount asc) as densamount from orders;
+select item,amount, row_number() over (order by amount asc) as rn from orders;
